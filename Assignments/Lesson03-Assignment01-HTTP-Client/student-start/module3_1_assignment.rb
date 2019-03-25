2352d2f3ca4a58620fa5a4fbcddaa5ee330042d4
Jonathan <38994980+jhnsolis@users.noreply.github.com>
2019-03-24 22:23:11 -0600

    Solve the assignment Don't forget to add convert the result to an array ["recipes"] if you don't do that and insted use: get("/search", @options) You will receive the raw json data

--- Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,21 @@
 require 'httparty'
 
 class Recipe
+    include HTTParty
+
+    @key = "f03cc67b6c4e6b304ae12614475fa5b5"
+    ENV["FOOD2FORK_KEY"] = @key
+
+    default_options.update(verify: false) #turn off SLL verification
+    default_params.update(key: @key)    
+
+    base_uri "http://food2fork.com/api"
+    #https://www.food2fork.com/api/search?key=YOUR_API_KEY&q=chicken%20breast
+    format :json
+
+    def self.for(keyword)
+        @options = {query: {q: keyword}}
+        get("/search", @options)["recipes"]
+    end
 
 end
